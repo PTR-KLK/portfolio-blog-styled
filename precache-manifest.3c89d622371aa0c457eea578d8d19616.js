56eb8419bb192a9731053dc4ee4b848b0d0dcc89
Piotr Kielak <p.kielak@outlook.com>
2020-05-04 00:18:51 +0200

    Updates

---
+++ precache-manifest.3c89d622371aa0c457eea578d8d19616.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b52ea40f56bec7b6a9c564b318d5274",
+    "url": "/portfolio-blog-styled/index.html"
+  },
+  {
+    "revision": "483c4c6d57ce52df44d1",
+    "url": "/portfolio-blog-styled/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "c75ca33cb079549c33f3",
+    "url": "/portfolio-blog-styled/static/css/main.72bb1536.chunk.css"
+  },
+  {
+    "revision": "483c4c6d57ce52df44d1",
+    "url": "/portfolio-blog-styled/static/js/2.64d6f39a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-blog-styled/static/js/2.64d6f39a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c75ca33cb079549c33f3",
+    "url": "/portfolio-blog-styled/static/js/main.698f057a.chunk.js"
+  },
+  {
+    "revision": "923f43234376078f4755",
+    "url": "/portfolio-blog-styled/static/js/runtime-main.aa38ac51.js"
+  },
+  {
+    "revision": "4788048f319dc48101678d9e69f5077e",
+    "url": "/portfolio-blog-styled/static/media/me.4788048f.png"
+  }
+]);
